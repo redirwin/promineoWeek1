61eac897b5e913240b82913156af0e015fb98114
Dave Irwin <david@redirwin.com>
2020-01-29 20:56:46 -0700

    created all variables and displaying results.

--- src/promineoWeek1/App.java
+++ src/promineoWeek1/App.java
@@ -3,8 +3,23 @@ package promineoWeek1;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		double itemPrice = 9.99;
+		double cashInWallet = 20.00;
+		int numberOfFriends = 45;
+		int ageInYears = 40;
+		String firstName = "David";
+		String lastName = "Irwin";
+		char middleInitial = 'J';
+		
+		double walletAfterPurchase = cashInWallet - itemPrice; 
+		double friendsMadePerYear = numberOfFriends / ageInYears;
+		String fullName = firstName + " " + middleInitial + ". " + lastName;
+		
+		System.out.println("I only have $" + walletAfterPurchase + " left after buying lunch." );
+		System.out.println("I make friends at the rate of about " + friendsMadePerYear + " per year.");
+		System.out.println("My full name is " + fullName + ".");
+		
 	}
 
 }
